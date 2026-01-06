9834d0d2a468e5804ea7a1d457e52598bae1067b
Eklak-Alam <Eklak-Alam@users.noreply.github.com>
2026-01-06 03:32:16 +0000

    🎨 Auto-formatted code via GitHub Actions

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    // Select all cards and arrows
-    const elements = document.querySelectorAll('.card, .arrow');
-    
-    // Animate them one by one with a delay
-    elements.forEach((el, index) => {
-        setTimeout(() => {
-            el.style.animation = "fadeIn 0.6s ease-out forwards";
-        }, index * 400); // 400ms delay between each item
-    });
+  // Select all cards and arrows
+  const elements = document.querySelectorAll(".card, .arrow");
 
-    console.log("Terraform Automation Dashboard Loaded.");
+  // Animate them one by one with a delay
+  elements.forEach((el, index) => {
+    setTimeout(() => {
+      el.style.animation = "fadeIn 0.6s ease-out forwards";
+    }, index * 400); // 400ms delay between each item
+  });
+
+  console.log("Terraform Automation Dashboard Loaded.");
 });
